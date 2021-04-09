b1a6b3dd2403c1d8ca1509f38b9321709a44a2f6
omar <omar11alhaj@gmail.com>
2021-04-09 03:26:50 +0300

    adding simple doubly linked list operation

---
+++ DLL.py
@@ -0,0 +1,60 @@
+class Node:
+    def __init__(self, value=None):
+        self.prev = None
+        self.value = value
+        self.next = None
+
+
+# DLL operations class
+class Doubly_Linked_list:
+
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def __iter__(self):
+        temp_node = self.head
+        while temp_node:
+            yield temp_node
+            temp_node = temp_node.next
+            if temp_node == self.tail.next:
+                break
+
+    def dll_creation(self, value):
+        if (self.head == None and self.tail == None):
+            new_node = Node(value)
+            self.head = new_node
+            self.tail = new_node
+            print('list created!')
+        else:
+            print("DLL already exists!")
+
+    def insert_to_dll(self, value, location):
+        if self.head == None and self.tail == None:
+            return ('This list doesn`t exist!')
+        else:
+            new_node = Node(value)
+            if location == 0:
+                new_node.prev = None
+                new_node.next = self.head
+                self.head.prev = new_node
+                self.head = new_node
+            elif location == -1:
+                new_node.next = None
+                new_node.prev = self.tail
+                self.tail.next = new_node
+                self.tail = new_node
+            else:
+                temp_node = self.head
+                index = 0
+                while index < location - 1:
+                    temp_node = temp_node.next
+                    index += 1
+                next_node = temp_node.next
+                temp_node.next = new_node
+                new_node.prev = temp_node
+                new_node.next = next_node
+                if next_node is None:
+                    self.tail = new_node
+                else:
+                    next_node.prev = new_node
